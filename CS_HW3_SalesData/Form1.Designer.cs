02329f97d22d894fee1733d4a159df14ecc1cbf3
errornosignal <errornosignal@users.noreply.github.com>
2018-03-05 11:45:58 -0600

    03 05 2018 11:45 working class

--- CS_HW3_SalesData/Form1.Designer.cs
+++ CS_HW3_SalesData/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.SalesDataGroupBox = new System.Windows.Forms.GroupBox();
-            this.RepNameLabel = new System.Windows.Forms.Label();
-            this.SaleAmtLabel = new System.Windows.Forms.Label();
-            this.SaleCountLabel = new System.Windows.Forms.Label();
-            this.RepNameTextBox = new System.Windows.Forms.TextBox();
-            this.SaleAmtTextBox = new System.Windows.Forms.TextBox();
-            this.AddSaleButton = new System.Windows.Forms.Button();
             this.SaleCountOutputLabel = new System.Windows.Forms.Label();
+            this.AddSaleButton = new System.Windows.Forms.Button();
+            this.SaleAmtTextBox = new System.Windows.Forms.TextBox();
+            this.RepNameTextBox = new System.Windows.Forms.TextBox();
+            this.SaleCountLabel = new System.Windows.Forms.Label();
+            this.SaleAmtLabel = new System.Windows.Forms.Label();
+            this.RepNameLabel = new System.Windows.Forms.Label();
             this.HighestButton = new System.Windows.Forms.Button();
             this.NumToReturnTextBox = new System.Windows.Forms.TextBox();
             this.LowestButton = new System.Windows.Forms.Button();
@@ -43,7 +43,7 @@
             this.ClearSalesDataButton = new System.Windows.Forms.Button();
             this.ResetButton = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
-            this.OutputListView = new System.Windows.Forms.ListView();
+            this.OutputListBox = new System.Windows.Forms.ListBox();
             this.SalesDataGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,23 +63,38 @@
             this.SalesDataGroupBox.TabStop = false;
             this.SalesDataGroupBox.Text = "Sales Data";
             // 
-            // RepNameLabel
+            // SaleCountOutputLabel
             // 
-            this.RepNameLabel.AutoSize = true;
-            this.RepNameLabel.Location = new System.Drawing.Point(13, 31);
-            this.RepNameLabel.Name = "RepNameLabel";
-            this.RepNameLabel.Size = new System.Drawing.Size(61, 13);
-            this.RepNameLabel.TabIndex = 0;
-            this.RepNameLabel.Text = "Rep Name:";
+            this.SaleCountOutputLabel.AutoSize = true;
+            this.SaleCountOutputLabel.Location = new System.Drawing.Point(164, 79);
+            this.SaleCountOutputLabel.Name = "SaleCountOutputLabel";
+            this.SaleCountOutputLabel.Size = new System.Drawing.Size(16, 13);
+            this.SaleCountOutputLabel.TabIndex = 7;
+            this.SaleCountOutputLabel.Text = "---";
             // 
-            // SaleAmtLabel
+            // AddSaleButton
             // 
-            this.SaleAmtLabel.AutoSize = true;
-            this.SaleAmtLabel.Location = new System.Drawing.Point(4, 56);
-            this.SaleAmtLabel.Name = "SaleAmtLabel";
-            this.SaleAmtLabel.Size = new System.Drawing.Size(70, 13);
-            this.SaleAmtLabel.TabIndex = 1;
-            this.SaleAmtLabel.Text = "Sale Amount:";
+            this.AddSaleButton.Location = new System.Drawing.Point(105, 102);
+            this.AddSaleButton.Name = "AddSaleButton";
+            this.AddSaleButton.Size = new System.Drawing.Size(75, 23);
+            this.AddSaleButton.TabIndex = 6;
+            this.AddSaleButton.Text = "Add Sale";
+            this.AddSaleButton.UseVisualStyleBackColor = true;
+            this.AddSaleButton.Click += new System.EventHandler(this.AddSaleButton_Click);
+            // 
+            // SaleAmtTextBox
+            // 
+            this.SaleAmtTextBox.Location = new System.Drawing.Point(80, 53);
+            this.SaleAmtTextBox.Name = "SaleAmtTextBox";
+            this.SaleAmtTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SaleAmtTextBox.TabIndex = 4;
+            // 
+            // RepNameTextBox
+            // 
+            this.RepNameTextBox.Location = new System.Drawing.Point(80, 27);
+            this.RepNameTextBox.Name = "RepNameTextBox";
+            this.RepNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RepNameTextBox.TabIndex = 3;
             // 
             // SaleCountLabel
             // 
@@ -90,37 +105,23 @@
             this.SaleCountLabel.TabIndex = 2;
             this.SaleCountLabel.Text = "Sale Count:";
             // 
-            // RepNameTextBox
+            // SaleAmtLabel
             // 
-            this.RepNameTextBox.Location = new System.Drawing.Point(80, 28);
-            this.RepNameTextBox.Name = "RepNameTextBox";
-            this.RepNameTextBox.Size = new System.Drawing.Size(100, 20);
-            this.RepNameTextBox.TabIndex = 3;
+            this.SaleAmtLabel.AutoSize = true;
+            this.SaleAmtLabel.Location = new System.Drawing.Point(4, 56);
+            this.SaleAmtLabel.Name = "SaleAmtLabel";
+            this.SaleAmtLabel.Size = new System.Drawing.Size(70, 13);
+            this.SaleAmtLabel.TabIndex = 1;
+            this.SaleAmtLabel.Text = "Sale Amount:";
             // 
-            // SaleAmtTextBox
+            // RepNameLabel
             // 
-            this.SaleAmtTextBox.Location = new System.Drawing.Point(80, 53);
-            this.SaleAmtTextBox.Name = "SaleAmtTextBox";
-            this.SaleAmtTextBox.Size = new System.Drawing.Size(100, 20);
-            this.SaleAmtTextBox.TabIndex = 4;
-            // 
-            // AddSaleButton
-            // 
-            this.AddSaleButton.Location = new System.Drawing.Point(105, 102);
-            this.AddSaleButton.Name = "AddSaleButton";
-            this.AddSaleButton.Size = new System.Drawing.Size(75, 23);
-            this.AddSaleButton.TabIndex = 6;
-            this.AddSaleButton.Text = "Add Sale";
-            this.AddSaleButton.UseVisualStyleBackColor = true;
-            // 
-            // SaleCountOutputLabel
-            // 
-            this.SaleCountOutputLabel.AutoSize = true;
-            this.SaleCountOutputLabel.Location = new System.Drawing.Point(164, 79);
-            this.SaleCountOutputLabel.Name = "SaleCountOutputLabel";
-            this.SaleCountOutputLabel.Size = new System.Drawing.Size(16, 13);
-            this.SaleCountOutputLabel.TabIndex = 7;
-            this.SaleCountOutputLabel.Text = "---";
+            this.RepNameLabel.AutoSize = true;
+            this.RepNameLabel.Location = new System.Drawing.Point(13, 31);
+            this.RepNameLabel.Name = "RepNameLabel";
+            this.RepNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.RepNameLabel.TabIndex = 0;
+            this.RepNameLabel.Text = "Rep Name:";
             // 
             // HighestButton
             // 
@@ -183,20 +184,21 @@
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = true;
             // 
-            // OutputListView
+            // OutputListBox
             // 
-            this.OutputListView.Location = new System.Drawing.Point(216, 12);
-            this.OutputListView.Name = "OutputListView";
-            this.OutputListView.Size = new System.Drawing.Size(203, 278);
-            this.OutputListView.TabIndex = 8;
-            this.OutputListView.UseCompatibleStateImageBehavior = false;
+            this.OutputListBox.FormattingEnabled = true;
+            this.OutputListBox.Location = new System.Drawing.Point(218, 12);
+            this.OutputListBox.Name = "OutputListBox";
+            this.OutputListBox.Size = new System.Drawing.Size(199, 277);
+            this.OutputListBox.TabIndex = 8;
             // 
             // Form1
             // 
+            this.AcceptButton = this.AddSaleButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(429, 300);
-            this.Controls.Add(this.OutputListView);
+            this.Controls.Add(this.OutputListBox);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.ResetButton);
             this.Controls.Add(this.ClearSalesDataButton);
@@ -231,7 +233,7 @@
         private System.Windows.Forms.Button ClearSalesDataButton;
         private System.Windows.Forms.Button ResetButton;
         private System.Windows.Forms.Button ExitButton;
-        private System.Windows.Forms.ListView OutputListView;
+        private System.Windows.Forms.ListBox OutputListBox;
     }
 }
 
--- CS_HW3_SalesData/Form1.cs
+++ CS_HW3_SalesData/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,8 +18,69 @@ namespace CS_HW3_SalesData
             InitializeComponent();
         }
 
+        List <Sale> SalesList = new List<Sale>();
 
-        //Write some code
+        //sort with
+        ///!!!//SalesList.OrderBy(x => x.SaleAmt);
+        //var sortedList = SalesList.OrderBy(i => i.SaleAmt);
+
+        int SaleNumber = 0;
+
+        private void AddSaleButton_Click(object sender, EventArgs e)
+        {
+            var RepName = RepNameTextBox.Text;
+            var isValid = decimal.TryParse(SaleAmtTextBox.Text, out decimal NewDecimal);
+
+            if (!isValid)
+            {
+                //display error on invalid amount entered
+                MessageBox.Show("Sale amount was not a number.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                SaleAmtTextBox.Clear();
+                SaleAmtTextBox.Focus();
+            }
+            else
+            {
+                //add new Sale object
+                SalesList.Add(new Sale(SaleNumber, RepName, NewDecimal));
+                SaleNumber++;
+
+                //update sale count label
+                SaleCountOutputLabel.Text = SaleNumber.ToString();
+
+                //clear and focus
+                RepNameTextBox.Clear();
+                SaleAmtTextBox.Clear();
+                RepNameTextBox.Focus();
+
+                //reset datasource
+                OutputListBox.DataSource = null;
+                OutputListBox.DataSource = SalesList;
+            }
+            
+        }
+
+        /// <summary>
+        /// Sale class
+        /// </summary>
+        public class Sale
+        {
+            public Sale(int saleNumber, string repName, decimal saleAmt)
+            {
+                this.SaleNumber = saleNumber;
+                this.RepName = repName;
+                this.SaleAmt = saleAmt;
+            }
+
+            public int SaleNumber { get; set; }
+            public string RepName { get; set; }
+            public decimal SaleAmt { get; set; }
+
+            public override string ToString()
+            {
+                return RepName + " " + SaleAmt;
+            }
+        }
 
     }
 }
